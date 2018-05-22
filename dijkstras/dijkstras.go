4c9c5428fdd6953b9ba8250645673292b30c0e79
Luke Brady <lbrad23105@gmail.com>
2018-05-22 16:48:37 -0400

    Still working on this.

--- dijkstras/dijkstras.go
+++ dijkstras/dijkstras.go
@@ -2,7 +2,29 @@ package main
 
 // Graph datastructure
 type Graph struct {
-	size uint
+	size  uint
+	edges map[string][]Node
+}
+
+// Node that holds data within the graph.
+type Node struct {
+	name   string
+	weight uint
+	data   []byte
+}
+
+// NewGraph produces a new graph object.
+func NewGraph() *Graph {
+	var edgeArr map[string][]Node
+	return &Graph{
+		size:  0,
+		edges: edgeArr,
+	}
+}
+
+// AddNode adds a data node the graph.
+func (g *Graph) AddNode(name string, neighbors []Node, weight uint) {
+	// Check to see if the node exists in the graph.
 }
 
 func main() {
